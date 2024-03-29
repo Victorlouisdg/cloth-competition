915b4b70e79b621b5b0329dfc9298d8d9c942615
victorlouisdg <victorlouisdg@gmail.com>
2024-03-29 16:06:15 +0100

    script to collect reference images

--- scripts/data_collection/09_data_collection_loop.py
+++ scripts/data_collection/09_data_collection_loop.py
@@ -2,8 +2,6 @@ import time
 from pathlib import Path
 
 from airo_camera_toolkit.cameras.multiprocess.multiprocess_video_recorder import MultiprocessVideoRecorder
-from airo_camera_toolkit.interfaces import StereoRGBDCamera
-from airo_camera_toolkit.point_clouds.conversions import open3d_to_point_cloud, point_cloud_to_open3d
 from cloth_tools.bounding_boxes import BBOX_CLOTH_IN_THE_AIR, BBOX_CLOTH_ON_TABLE
 from cloth_tools.controllers.grasp_hanging_controller import GraspHangingController
 from cloth_tools.controllers.grasp_highest_controller import GraspHighestController
@@ -11,70 +9,14 @@ from cloth_tools.controllers.grasp_lowest_controller import GraspLowestControlle
 from cloth_tools.controllers.home_controller import HomeController
 from cloth_tools.controllers.stretch_controller import StretchController
 from cloth_tools.dataset.bookkeeping import ensure_dataset_dir, find_highest_suffix
-from cloth_tools.dataset.format import CompetitionObservation, save_competition_observation
+from cloth_tools.dataset.collection import collect_observation
+from cloth_tools.dataset.format import save_competition_observation
 from cloth_tools.motion_blur_detector import MotionBlurDetector
 from cloth_tools.point_clouds.cloth_detection import sufficient_points_in_bbox
 from cloth_tools.stations.competition_station import CompetitionStation
 from loguru import logger
 
-
-def collect_observation(station: CompetitionStation) -> str:
-    image_left = camera.get_rgb_image_as_int()
-    image_right = camera._retrieve_rgb_image_as_int(StereoRGBDCamera.RIGHT_RGB)
-    depth_map = camera._retrieve_depth_map()
-    depth_image = camera._retrieve_depth_image()
-    confidence_map = camera._retrieve_confidence_map()
-
-    point_cloud_in_camera = camera._retrieve_colored_point_cloud()
-    pcd_in_camera = point_cloud_to_open3d(point_cloud_in_camera)
-    pcd = pcd_in_camera.transform(camera_pose_in_world)  # transform to world frame (= base frame of left robot)
-    point_cloud = open3d_to_point_cloud(pcd)
-
-    arm_left_tcp_pose_in_base = station.dual_arm.left_manipulator.get_tcp_pose()
-    arm_left_joints = station.dual_arm.left_manipulator.get_joint_configuration()
-    arm_right_tcp_pose_in_base = station.dual_arm.right_manipulator.get_tcp_pose()
-    arm_right_joints = station.dual_arm.right_manipulator.get_joint_configuration()
-
-    # Convert TCP poses from robot base frames to world frame
-    X_LCB_TCPL = arm_left_tcp_pose_in_base
-    X_RCB_TCPR = arm_right_tcp_pose_in_base
-
-    X_W_LCB = arm_left_pose_in_world
-    X_W_RCB = arm_right_pose_in_world
-
-    X_W_TCPL = X_W_LCB @ X_LCB_TCPL
-    X_W_TCPR = X_W_RCB @ X_RCB_TCPR
-
-    arm_left_tcp_pose_in_world = X_W_TCPL
-    arm_right_tcp_pose_in_world = X_W_TCPR
-
-    observation = CompetitionObservation(
-        image_left=image_left,
-        image_right=image_right,
-        depth_map=depth_map,
-        point_cloud=point_cloud,
-        depth_image=depth_image,
-        confidence_map=confidence_map,
-        camera_pose_in_world=camera_pose_in_world,
-        arm_left_pose_in_world=arm_left_pose_in_world,
-        arm_right_pose_in_world=arm_right_pose_in_world,
-        arm_left_joints=arm_left_joints,
-        arm_right_joints=arm_right_joints,
-        arm_left_tcp_pose_in_world=arm_left_tcp_pose_in_world,
-        arm_right_tcp_pose_in_world=arm_right_tcp_pose_in_world,
-        right_camera_pose_in_left_camera=right_camera_pose_in_left_camera,
-        camera_intrinsics=camera_intrinsics,
-        camera_resolution=camera_resolution,
-    )
-    return observation
-
-
 if __name__ == "__main__":
-    # import os
-
-    # os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-    # os.environ["CUDA_VISIBLE_DEVICES"] = "3"
-
     station = CompetitionStation()
 
     dual_arm = station.dual_arm
@@ -139,6 +81,8 @@ if __name__ == "__main__":
         observation_start = collect_observation(station)
         save_competition_observation(observation_start, observation_start_dir)
 
+        # TODO attempt GraspHaningController to accept observation_start as input
+        # Currently, it will retrieve the next camera frame, which is very slightly different
         grasp_hanging_controller = GraspHangingController(station, BBOX_CLOTH_IN_THE_AIR, sample_dir)
         grasp_hanging_controller.execute(interactive=False)
 
