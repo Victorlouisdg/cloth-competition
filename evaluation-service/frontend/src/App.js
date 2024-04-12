4635d91e52a6a3ea6596ca3c5ab20fb84da8de84
julius <julius@nimble.ai>
2024-04-12 13:48:10 -0700

    fix: add tailwind styles

--- evaluation-service/frontend/src/App.js
+++ evaluation-service/frontend/src/App.js
@@ -1,23 +1,39 @@
 // App.js
-import './App.css';
-import React, { useState, useEffect } from 'react';
-import { Stage, Layer, Image, Line, Text } from 'react-konva';
-import Konva from 'konva';
-import axios from 'axios';
-import Select from 'react-select';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import { Stage, Layer, Image, Line, Text } from "react-konva";
+import Konva from "konva";
+import axios from "axios";
+import Select from "react-select";
 import Switch from "react-switch";
 import ClipLoader from "react-spinners/ClipLoader";
-import Modal from 'react-modal';
+import Modal from "react-modal";
+
+const customStyles = {
+  option: (provided, state) => ({
+    ...provided,
+    color: 'black',
+    backgroundColor: state.isSelected ? 'lightGrey' : 'white',
+  }),
+  control: (provided) => ({
+    ...provided,
+    backgroundColor: 'white',
+    color: 'black',
+  }),
+};
 
 var ColorReplaceFilter = function (imageData) {
-  console.log('Applying filter');
+  console.log("Applying filter");
   var nPixels = imageData.data.length;
   for (var i = 0; i < nPixels; i += 4) {
-    const isWhite = imageData.data[i] === 255 && imageData.data[i + 1] === 255 && imageData.data[i + 2] === 255;
+    const isWhite =
+      imageData.data[i] === 255 &&
+      imageData.data[i + 1] === 255 &&
+      imageData.data[i + 2] === 255;
     if (isWhite) {
-      imageData.data[i] = 0;     // Red
+      imageData.data[i] = 0; // Red
       imageData.data[i + 1] = 255; // Green
-      imageData.data[i + 2] = 0;   // Blue
+      imageData.data[i + 2] = 0; // Blue
     }
   }
 };
@@ -46,7 +62,6 @@ const App = () => {
   }, [maskImage, showMask]);
 
   const fetchSceneImageAndMaybeMask = async (currentScene) => {
-
     setIsLoading(true);
 
     try {
@@ -54,8 +69,10 @@ const App = () => {
 
       if (currentScene.maskExists) {
         const maskImg = new window.Image();
-        maskImg.src = `http://127.0.0.1:5000/scenes/${currentScene.sceneName}/mask?${Date.now()}`;
-        maskImg.crossOrigin = 'Anonymous';
+        maskImg.src = `http://127.0.0.1:5000/scenes/${
+          currentScene.sceneName
+        }/mask?${Date.now()}`;
+        maskImg.crossOrigin = "Anonymous";
         maskImg.onload = () => {
           const scale = maxWidth / maskImg.width;
           setMaskImage({ img: maskImg, scale });
@@ -64,7 +81,7 @@ const App = () => {
 
       const img = new window.Image();
       img.src = `http://127.0.0.1:5000/scenes/${currentScene.sceneName}/image`;
-      img.crossOrigin = 'Anonymous';
+      img.crossOrigin = "Anonymous";
       img.onload = () => {
         const scale = maxWidth / img.width;
         setImage({ img, scale });
@@ -72,18 +89,17 @@ const App = () => {
 
       const depthImg = new window.Image();
       depthImg.src = `http://127.0.0.1:5000/scenes/${currentScene.sceneName}/depth`;
-      depthImg.crossOrigin = 'Anonymous';
+      depthImg.crossOrigin = "Anonymous";
       depthImg.onload = () => {
         const scale = maxWidth / depthImg.width;
         setDepthImage({ img: depthImg, scale });
       };
-      
     } catch (error) {
-      console.error('Error fetching scene image:', error);
+      console.error("Error fetching scene image:", error);
     } finally {
       setIsLoading(false);
     }
-  }
+  };
 
   const fetchAvailableFiles = async () => {
     setIsLoading(true);
@@ -91,20 +107,18 @@ const App = () => {
       const initialAvailableFiles = await getAvailableFiles();
       setAvailableFiles(initialAvailableFiles);
     } catch (error) {
-      console.error('Error fetching available files:', error);
+      console.error("Error fetching available files:", error);
     } finally {
       setIsLoading(false);
     }
-    
-  }
-
+  };
 
   const handleRefresh = async () => {
     fetchAvailableFiles();
     if (scene) {
       fetchSceneImageAndMaybeMask(scene);
     }
-  }
+  };
 
   // useEffect(() => {
   //   let intervalId;
@@ -128,10 +142,10 @@ const App = () => {
   //   }
 
   //   startInterval();
-  
+
   //   return () => {
   //     if (intervalId) {
-  //       clearInterval(intervalId); 
+  //       clearInterval(intervalId);
   //     }
   //   };
   // }, [autoLoad, scene])
@@ -144,15 +158,14 @@ const App = () => {
     if (scene) {
       fetchSceneImageAndMaybeMask(scene);
     }
-    
   }, [scene]);
 
   const getAvailableFiles = async () => {
     try {
-      const response = await axios.get('http://127.0.0.1:5000/api/scenes');
-      return response.data.scenes
+      const response = await axios.get("http://127.0.0.1:5000/api/scenes");
+      return response.data.scenes;
     } catch (error) {
-      console.error('Error fetching available files:', error);
+      console.error("Error fetching available files:", error);
       return [];
     }
   };
@@ -166,135 +179,162 @@ const App = () => {
     setHoverPosition({ x: layerX, y: layerY });
   };
 
-
   const handleImageClick = (event) => {
     const { layerX, layerY } = event.evt;
-    console.log('layerX:', layerX, 'layerY:', layerY);
-    axios.post('http://127.0.0.1:5000/api/annotate', { x: layerX / image.scale,  y: layerY / image.scale, sceneName: scene.sceneName})
-      .then(response => {
+    console.log("layerX:", layerX, "layerY:", layerY);
+    axios
+      .post("http://127.0.0.1:5000/api/annotate", {
+        x: layerX / image.scale,
+        y: layerY / image.scale,
+        sceneName: scene.sceneName,
+      })
+      .then((response) => {
         fetchSceneImageAndMaybeMask(scene);
       })
-      .catch(error => {
-        console.error('Error fetching coordinates:', error);
-      })
+      .catch((error) => {
+        console.error("Error fetching coordinates:", error);
+      });
   };
 
-  const selectOptions = availableFiles.map(file => ({ value: file, label: file.sceneName }));
+  const selectOptions = availableFiles.map((file) => ({
+    value: file,
+    label: file.sceneName,
+  }));
 
   return (
-  <>
-    <Modal
-      isOpen={isLoading}
-      contentLabel="Loading Modal"
-      ariaHideApp={false}
-      style={{
-        overlay: {
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          backgroundColor: 'rgba(0, 0, 0, 0.5)'
-        },
-        content: {
-          position: 'absolute',
-          top: '50%',
-          left: '50%',
-          background: 'none',
-          border: 'none',
-          padding: '0'
-        }
-      }}
-    >
+    <>
+      <Modal
+        isOpen={isLoading}
+        contentLabel="Loading Modal"
+        ariaHideApp={false}
+        style={{
+          overlay: {
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            backgroundColor: "rgba(0, 0, 0, 0.5)",
+          },
+          content: {
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            background: "none",
+            border: "none",
+            padding: "0",
+          },
+        }}
+      >
         <ClipLoader color="#ffffff" loading={true} size={30} />
-    </Modal>
-    
-    
-      <div style={{ display: 'flex', flexDirection: 'row'}}>
-      <img style={{height: 60, width: 100}} src={process.env.PUBLIC_URL + '/ICRA_2024.jpg'} alt="Logo" className="logo" />
-      
-      <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'space-between' }}>
-      <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
-        <Select
-            value={selectOptions.find(option => option.value.sceneName === scene?.sceneName)}
-            onChange={handleChange}
-            options={selectOptions}
-            placeholder="Select data"
-            styles={{
-              container: (provided, state) => ({
-                ...provided,
-                width: '200px',
-              }),
+      </Modal>
+
+      <div className="flex items-center justify-between p-4 bg-gray-800 text-white font-sans">
+        <img
+          className="h-16 w-24"
+          src={process.env.PUBLIC_URL + "/ICRA_2024.jpg"}
+          alt="Logo"
+        />
+
+        <div className="flex items-center justify-between space-x-4">
+          <div className="flex items-center space-x-4">
+            <Select
+              value={selectOptions.find(
+                (option) => option.value.sceneName === scene?.sceneName
+              )}
+              onChange={handleChange}
+              options={selectOptions}
+              placeholder="Select data"
+              styles={customStyles}
+            />
+
+            <button
+              className="px-4 py-2 bg-blue-500 hover:bg-blue-700 text-white font-bold rounded"
+              onClick={handleRefresh}
+            >
+              Refresh
+            </button>
+          </div>
+
+          <div className="flex items-center space-x-4">
+            <Switch
+              onChange={() => {
+                setShowMask(!showMask);
+              }}
+              checked={showMask}
+            />
+            <div>Show Mask</div>
+            <Switch
+              onChange={() => setShowDepth(!showDepth)}
+              checked={showDepth}
+            />
+            <div>Show Depth</div>
+
+            <Switch
+              onChange={() => setAutoLoad(!autoLoad)}
+              checked={autoLoad}
+            />
+            <div>Auto check for new data</div>
+          </div>
+        </div>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        {!!image && (
+          <div
+            style={{
+              marginTop: "20px",
+              border: "1px solid #ccc",
+              borderRadius: "5px",
+              padding: "10px",
             }}
-          />
-
-        <button className="refresh-button" onClick={handleRefresh}>Refresh</button>
+          >
+            <Stage
+              onMouseMove={handleMouseMove}
+              width={image ? image.img.width * image.scale : 800}
+              height={image ? image.img.height * image.scale : 600}
+            >
+              <Layer>
+                {image && !showDepth && (
+                  <Image
+                    image={image.img}
+                    onClick={handleImageClick}
+                    scaleX={image.scale}
+                    scaleY={image.scale}
+                  />
+                )}
+                {depthImage && showDepth && (
+                  <Image
+                    image={depthImage.img}
+                    onClick={handleImageClick}
+                    scaleX={depthImage.scale}
+                    scaleY={depthImage.scale}
+                  />
+                )}
+                {maskImage && showMask && (
+                  <Image
+                    onClick={handleImageClick}
+                    image={maskImage.img}
+                    opacity={0.3}
+                    scaleX={maskImage.scale}
+                    scaleY={maskImage.scale}
+                    filters={[ColorReplaceFilter]}
+                    ref={maskImageRef}
+                  />
+                )}
+                <HoverText
+                  x={hoverPosition.x}
+                  y={hoverPosition.y}
+                  scale={image.scale}
+                />
+              </Layer>
+            </Stage>
+          </div>
+        )}
       </div>
-
-      <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
-        <Switch 
-            onChange={() => {
-              setShowMask(!showMask)
-            }} 
-            checked={showMask} 
-          />
-        <div className="switch-label">
-          Show Mask
-        </div>
-        <Switch 
-            onChange={() => setShowDepth(!showDepth)} 
-            checked={showDepth} 
-          />
-        <div className="switch-label">
-          Show Depth
-        </div>
-        
-        <Switch 
-            onChange={() => setAutoLoad(!autoLoad)} 
-            checked={autoLoad} 
-          />
-        <div className="switch-label">
-          Auto check for new data
-        </div>
-      </div>
-      </div>
-      
-      
-      </div>
-      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-      {!!image && (<div style={{ marginTop: '20px', border: '1px solid #ccc', borderRadius: '5px', padding: '10px' }}>
-        <Stage onMouseMove={handleMouseMove} width={image ? image.img.width * image.scale : 800} height={image ? image.img.height * image.scale : 600}>
-          <Layer>
-            {image && !showDepth && (
-              <Image
-                image={image.img}
-                onClick={handleImageClick}
-                scaleX={image.scale}
-                scaleY={image.scale}
-              />
-            )}
-            {depthImage && showDepth && (
-              <Image
-                image={depthImage.img}
-                onClick={handleImageClick}
-                scaleX={depthImage.scale}
-                scaleY={depthImage.scale}
-              />
-            )}
-            {maskImage && showMask && (
-              <Image
-                onClick={handleImageClick}
-                image={maskImage.img}
-                opacity={0.3}
-                scaleX={maskImage.scale}
-                scaleY={maskImage.scale}
-                filters={[ColorReplaceFilter]}
-                ref={maskImageRef}
-              />
-            )}
-            <HoverText x={hoverPosition.x} y={hoverPosition.y} scale={image.scale} />
-          </Layer>
-        </Stage>
-      </div>)}
-    </div>
     </>
   );
 };
@@ -302,17 +342,16 @@ const App = () => {
 const HoverText = ({ x, y, scale }) => {
   return (
     <>
-    <Text
-      text={`Pixel: (${(x/scale).toFixed(2)}, ${(y/scale).toFixed(2)})`}
-      x={x - 150}
-      y={y - 30}
-      fontSize={12}
-      backgroundColor="white"
-      fill="green" // Set the fill color to red
-    />
+      <Text
+        text={`Pixel: (${(x / scale).toFixed(2)}, ${(y / scale).toFixed(2)})`}
+        x={x - 150}
+        y={y - 30}
+        fontSize={12}
+        backgroundColor="white"
+        fill="green" // Set the fill color to red
+      />
     </>
   );
 };
-
 
 export default App;
---
+++ evaluation-service/frontend/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['Lato', 'sans-serif'],
+      },
+      colors: {
+        'dropdown-color': '#333333', // replace with the color you want
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
